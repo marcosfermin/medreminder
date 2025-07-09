bba9f320fd3637b150d25b2dd810c57d346fa252
Marcos Fermin <marcosdavid1794@gmail.com>
2025-07-08 23:37:47 -0400

    Add CreatesApplication trait for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,11 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * Base test case for the application.
+ */
+
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
 }
